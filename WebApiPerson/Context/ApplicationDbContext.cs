e9f31cf9feca51019ca5e3c816e2a2e8c9bc4fe4
Ezequiel Feola <ezefeola@gmail.com>
2024-05-31 18:13:07 -0300

    API Persons

---
+++ WebApiPerson/Context/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Models;
+
+namespace WebApiPerson.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
